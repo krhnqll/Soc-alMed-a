8baa4aa62ff793da9735176f4c8f26c284c2b5f9
Karahan <92548316+krhnqll@users.noreply.github.com>
2024-01-06 16:50:32 +0300

    Update FunctionController.cs

--- SocıalMedıa/Controllers/FunctionController.cs
+++ SocıalMedıa/Controllers/FunctionController.cs
@@ -1,4 +1,4 @@
-﻿using SocıalMedıa.Models;
+using SocıalMedıa.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +12,9 @@ namespace SocıalMedıa.Controllers
 {
     public class FunctionController : Controller
     {
-        SocıalMedıaEntities Db = new SocıalMedıaEntities();
+        SocıalMedıaEntities1 Db = new SocıalMedıaEntities1();
         // GET: Function
+ 
         public ActionResult Index()
         {
             return View();
@@ -23,19 +24,40 @@ namespace SocıalMedıa.Controllers
         [HttpGet]
         public ActionResult AdminPage()
         {
-     
-            var CustomerList = Db.Customer.ToList();
-            var BillboardList = Db.Billboard.ToList();
+            var CustomerList = from c in Db.Customer
+                               join l in Db.Location
+                               on c.Loc_id equals l.Loc_id
+                               select new CusplusLocation
+                               {
+                                   Cus_id = c.Cus_id,
+                                   Cus_email = c.Cus_email,
+                                   Cus_name = c.Cus_name,
+                                   Cus_phonenumber = c.Cus_phonenumber,
+                                   Cus_surname = c.Cus_surname,
+                                   password = c.password,
+                                   username = c.username,
+                                   Loc_name = l.Loc_name
+                               };
+            var BillboardList = from b in Db.Billboard
+                                join l in Db.Location
+                                on b.Loc_id equals l.Loc_id
+                                select new BillplusLocation
+                                {
+                                    Billboard_id = b.Billboard_id,
+                                    Billboard_detail = b.Billboard_detail,
+                                    Billboard_height = b.Billboard_height,
+                                    Billboard_name = b.Billboard_name,
+                                    Billboard_status = b.Billboard_status,
+                                    Billboard_width = b.Billboard_width,
+                                    Loc_name = l.Loc_name
+                                };
 
             BillplusCus Model = new BillplusCus
             {
-               
                 Customer_List = CustomerList.ToList(),
                 Billboard_List = BillboardList.ToList(),
-                
-               
             };
-               
+
             return View(Model);
         }
 
@@ -43,30 +65,33 @@ namespace SocıalMedıa.Controllers
         [HttpPost]
         public ActionResult AddBillboard(Billboard bill_new)
         {
-            Billboard ekle = new Billboard {
-                Billboard_name = bill_new.Billboard_name,
-                Billboard_detail = bill_new.Billboard_detail,
-                Billboard_status = bill_new.Billboard_status,
-                Billboard_height = bill_new.Billboard_height,
-                Billboard_width = bill_new.Billboard_width,
-                
-               
-            };
-            Db.Billboard.Add(ekle);
-            Db.SaveChanges();
-            ViewBag.sonuc = "Ekleme Başarılı.";
+            
+    
+                if (ModelState.IsValid)
+            {
+                Db.Billboard.Add(bill_new);
+                Db.SaveChanges();
+                ViewBag.sonuc = "Ekleme Başarılı.";
 
-            return View();
+                return RedirectToAction("AdminPage");   
+            }
+                else
+            {
+
+
+                return View();
+            }
+            
         }
 
-        
+
         // Pano düzenleme kısmı
         [HttpGet]
         public ActionResult EditBillboard(int id)
         {
             var data = Db.Billboard.Find(id);
 
-            if(data == null)
+            if (data == null)
             {
                 return HttpNotFound();
             }
@@ -103,7 +128,7 @@ namespace SocıalMedıa.Controllers
         [HttpPost]
         public ActionResult EditCustomer(Customer model1)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 Db.Entry(model1).State = EntityState.Modified;
                 Db.SaveChanges();
@@ -115,7 +140,7 @@ namespace SocıalMedıa.Controllers
 
         //Billboard Silme kısmı
         public ActionResult DeleteBillboard(int? id)
-        {        
+        {
             var Billboard_data = Db.Billboard.Find(id);
             Db.Billboard.Remove(Billboard_data);
             Db.SaveChanges();
@@ -124,7 +149,7 @@ namespace SocıalMedıa.Controllers
         }
 
         // Kullanıcı silme kısmı
-        public ActionResult DeleteCustomer(int? id) 
+        public ActionResult DeleteCustomer(int? id)
         {
             var Customer_data = Db.Customer.Find(id);
             Db.Customer.Remove(Customer_data);
@@ -137,20 +162,20 @@ namespace SocıalMedıa.Controllers
         [HttpPost]
         public ActionResult AddCustomer(Customer new_cus)
         {
-            Customer ekle = new Customer {
-                Cus_name = new_cus.Cus_name,
-                Cus_surname = new_cus.Cus_surname,
-                Cus_email = new_cus.Cus_email,
-                Cus_phonenumber = new_cus.Cus_phonenumber,
-                username = new_cus.username,
-                password = new_cus.password,
-                Loc_id = new_cus.Loc_id
-                
-            };
-            Db.Customer.Add(ekle);
-            Db.SaveChanges();
-            ViewBag.sonuc = "Ekleme Başarılı.";
-            return View();
+          
+
+            if (ModelState.IsValid)
+            {
+                Db.Customer.Add(new_cus);
+                Db.SaveChanges();
+                ViewBag.sonuc = "Ekleme Başarılı.";
+
+                return RedirectToAction("AdminPage");
+            }
+            else
+            {
+                return View();
+            }
         }
         [HttpGet]
         public ActionResult AddBillboard()
@@ -158,18 +183,12 @@ namespace SocıalMedıa.Controllers
             return View();
         }
         [HttpGet]
-        public ActionResult AddCustomer()
+        public ActionResult AddCustomer(Location Loc_name)
         {
+            List<String> loc_names = Db.Location.Select(c => c.Loc_name).ToList();
+
+            ViewBag.Citylist = new SelectList(loc_names);
             return View();
-        }
-        public ActionResult Location_getir(BillplusCus billplus)
-        {
-            int locid = billplus.location.Loc_id;
-            Location location = Db.Location.FirstOrDefault(x => x.Loc_id == locid);
-
-            billplus.location = location;
-
-            return View(billplus);
         }
     }
 }
