b7d7815fa1ff5bf29f4d6155446cd196e9c961e1
Karahan <92548316+krhnqll@users.noreply.github.com>
2024-01-06 16:52:38 +0300

    Update CusplusLocation.cs

--- SocıalMedıa/Models/CusplusLocation.cs
+++ SocıalMedıa/Models/CusplusLocation.cs
@@ -1,14 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace SocıalMedıa.Models
 {
-    public class CusplusLocation
+    public class CusplusLocation 
     {
-        public List<Customer> Customer_List { get; internal set; }
+        public int Cus_id { get; set; }
+        public string Cus_name { get; set; }
+        public string Cus_surname { get; set; }
+        public string Cus_email { get; set; }
+        public string Cus_phonenumber { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string Loc_name { get; set; }
 
-        public List<Location> Location_List { get; internal set; }
+        public List<CusplusLocation> Customer_List { get; internal set; }
     }
 }
